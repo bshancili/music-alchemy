6c1766d013b2c5584d93514c6259da7d086728b2
Emoshimo <simsekemre35@gmail.com>
2023-11-08 16:09:20 +0300

    created drawer for homepage

---
+++ frontend/src/components/Drawer.js
@@ -0,0 +1,15 @@
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
+
+const Drawer = () => {
+  return <div>Drawer</div>;
+};
+
+export default Drawer;
