beab9f6806d1cb4dc801b2399e17b3ac8238dfb1
poyraz <poyrazglr@gmail.com>
2023-11-13 10:45:35 +0300

    modified code so info not included in json file will be added as NULL.

--- my-express-app/create-song.js
+++ my-express-app/create-song.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const serviceAccount = require('./music-alchemy-firebase-adminsdk.json');
 const bodyParser = require('body-parser');
 
+
 //initialize firebase
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -20,19 +21,58 @@ app.use(cors());
 // endpoint song request
 app.post('/song-request', async (req, res) => {
     console.log("Received Song Request:", req.body);
+    
+
+    const  acousticness= reqBody.acousticness || null;
+    const album_URL = reqBody.album_URL || null;
+    const album_release_date = reqBody.album_release_date || null;
+    const album_type = reqBody.album_type || null;
+    const album_images = reqBody.album_images || null;
+    const danceability = reqBody.danceability || null;
+    const  energy= reqBody.energy || null;
+    const instrumentalness = reqBody.instrumentalness || null;
+    const length_in_seconds = reqBody.length_in_seconds || null;
+    const liveness = reqBody.liveness || null;
+    const loudness = reqBody.loudness || null;
+    const  mode= reqBody.mode || null;
+    const rating = reqBody.rating || null;
+    const spotify_album_id = reqBody.spotify_album_id || null;
+    const spotify_track_id = reqBody.spotify_track_id || null;
+    const tempo = reqBody.tempo || null;      
+    const track_url = reqBody.track_url || null;
+    const valence = reqBody.valence || null;
+    
     const { songTitle, artist, album } = req.body; // Assuming the request body contains the song details
-  
-    // Add any necessary validation logic for the song request data here
+    
+
 
     
     try {
-      // Add logic here to save the song request to your database or perform any necessary operations
+      // Add logic here to save the song request to your databmore than ase or perform any necessary operations
       // For example, you can use Firebase Firestore to store the song request details
      
-      const songRequestRef = await db.collection('songRequests').add({ 
-        songTitle: songTitle,
-        artist: artist,
-        album: album
+      const songRequestRef = await db.collection('Tracks').add({ 
+        track_name: songTitle,
+        artists: artist,
+        album_name: album,
+        album_images: album_images,
+        acousticness: acousticness,
+        album_URL: album_URL,
+        album_release_date: album_release_date,
+        album_type: album_type,
+        danceability: danceability,
+        energy: energy,
+        instrumentalness: instrumentalness,
+        length_in_seconds: length_in_seconds,
+        liveness: liveness,
+        loudness: loudness,
+        mode: mode,
+        rating: rating,
+        spotify_album_id: spotify_album_id,
+        spotify_track_id: spotify_track_id,
+        tempo: tempo,
+        track_url: track_url,
+        valence: valence
       });
       // Send a success response with the saved song request details
       res.status(201).send({ 
