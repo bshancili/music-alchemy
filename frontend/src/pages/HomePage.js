22dc83dfe25f74240fb9d7d780c8499f964180cc
mertkuleci <77067371+mertkuleci@users.noreply.github.com>
2023-11-13 08:11:33 +0000

    homepage-profile

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,82 +1,309 @@
+// Import necessary Chakra UI components
 import React from "react";
-import { Container, Flex, Text, Spacer, Button, Image } from "@chakra-ui/react";
-import { Link } from "react-router-dom"; // Import Link from your routing library
+import styled from "styled-components";
 
-const HomePage = () => {
-  return (
-    <Container
-      maxW="100%"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-    >
-      <Flex
-        as="header"
-        width="100%"
-        justifyContent="space-between"
-        bg="purple.500" // Background color set to purple
-        color="yellow" // Text color set to yellow
-        p={4}
+import {
+  ChakraProvider,
+  Box,
+  Flex,
+  Heading,
+  Text,
+  extendTheme,
+  Link,
+  CSSReset,
+  Tooltip,
+} from "@chakra-ui/react";
+// Import your image files
+import homeIcon from "./images/home.svg";
+import musicIcon from "./images/music.svg";
+import penIcon from "./images/pen.svg";
+import envelopeIcon from "./images/envelope.svg";
+import userIcon from "./images/user.svg";
+
+// Extend the theme if needed
+const theme = extendTheme(/* Your theme configurations here */);
+// Header component with custom images as icons
+const Header = () => (
+  <Flex
+    as="header"
+    align="center"
+    justify="space-between"
+    p="35"
+    bg="#1A1E1F"
+    color="white"
+  >
+    <Flex align="center">
+      {/* Home Link */}
+      <Link
+        mt="64px"
+        mr="12px"
+        ml="138px"
+        href="/"
+        borderRadius="15px"
+        width="64px"
+        height="64px"
+        bg="#33373B5E"
+        _hover={{ bg: "#FFFFFF" }}
+        display="flex"
         alignItems="center"
+        justifyContent="center"
       >
-        <Image src="/logo.png" alt="Music Alchemy Logo" boxSize="100px" />
-        <Spacer />
-        <Link to="/profile">
-          <Button colorScheme="yellow" mr={2}>
-            My Profile
-          </Button>
+        <Tooltip label="Home" placement="bottom">
+          <img src={homeIcon} alt="Home" style={{ width: "100%" }} />
+        </Tooltip>
+      </Link>
+
+      {/* Recommend a Song Link */}
+      <Link
+        mt="64px"
+        href="/recommend"
+        borderRadius="15px"
+        width="64px"
+        height="64px"
+        bg="#33373B5E"
+        _hover={{ bg: "#FFFFFF" }}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Tooltip label="Recommend a Song" placement="bottom">
+          <img src={musicIcon} alt="Music" style={{ width: "100%" }} />
+        </Tooltip>
+      </Link>
+
+      {/* Search Bar */}
+      <Box flex="1" display="flex" alignItems="center" justifyContent="center">
+        <Link
+          mt="64px"
+          ml="12px"
+          mr="12px"
+          href="/search"
+          borderRadius="15px"
+          width="785px"
+          height="64px"
+          bg="#33373B5E"
+          _hover={{ bg: "#FFFFFF" }}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Tooltip label="Search" placement="bottom">
+            <div
+              style={{
+                width: "0px",
+                height: "40px",
+                backgroundColor: "#33373B5E",
+              }}
+            />
+          </Tooltip>
         </Link>
-      </Flex>
+      </Box>
 
-      <Flex flexDirection="row" alignItems="center" p={5}>
-        <Flex flexDirection="column" alignItems="center" flex={1}>
-          <Text fontSize="4xl" fontWeight="bold">
-            Playlists for You
-          </Text>
-          {/* Add your playlist items here */}
-        </Flex>
+      {/* Create Song Link */}
+      <Link
+        mr="12px"
+        mt="64px"
+        href="/create"
+        borderRadius="15px"
+        width="64px"
+        height="64px"
+        bg="#33373B5E"
+        _hover={{ bg: "#FFFFFF" }}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Tooltip label="Create Song" placement="bottom">
+          <img src={penIcon} alt="Pen" style={{ width: "100%" }} />
+        </Tooltip>
+      </Link>
 
-        <Flex flexDirection="column" alignItems="center" flex={5}>
-          <Text fontSize="4xl" fontWeight="bold">
-            Discover New Music
-          </Text>
-          <Text fontSize="xl" mt={2}>
-            Explore a vast library of songs and artists.
-          </Text>
-          <Button colorScheme="yellow" size="lg" mt={4}>
-            Get Started
-          </Button>
-        </Flex>
+      {/* Direct Message Link */}
+      <Link
+        mt="64px"
+        mr="12px"
+        href="/messages"
+        borderRadius="15px"
+        width="64px"
+        height="64px"
+        bg="#33373B5E"
+        _hover={{ bg: "#FFFFFF" }}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Tooltip label="Direct Message" placement="bottom">
+          <img src={envelopeIcon} alt="Envelope" style={{ width: "100%" }} />
+        </Tooltip>
+      </Link>
 
-        <Flex flexDirection="column" alignItems="center" flex={3}>
-          <Text fontSize="4xl" fontWeight="bold">
-            Create Playlists
-          </Text>
-          <Text fontSize="xl" mt={2}>
-            Build and share your favorite playlists.
-          </Text>
-        </Flex>
+      {/* My Profile Link */}
+      <Link
+        mt="64px"
+        mr="137px"
+        href="/profile"
+        borderRadius="15px"
+        width="64px"
+        height="64px"
+        bg="#33373B5E"
+        _hover={{ bg: "#FFFFFF" }}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Tooltip label="My Profile" placement="bottom">
+          <img src={userIcon} alt="User" style={{ width: "100%" }} />
+        </Tooltip>
+      </Link>
+    </Flex>
+  </Flex>
+);
 
-        <Flex flexDirection="column" alignItems="center" flex={3}>
-          <Text fontSize="4xl" fontWeight="bold">
-            Liked Songs
-          </Text>
-          <Text fontSize="xl" mt={2}>
-            Explore your favorite liked songs.
-          </Text>
-        </Flex>
-      </Flex>
+const MainSection = () => (
+  <Flex
+    as="main"
+    align="center"
+    justify="center"
+    p="4"
+    style={{
+      backgroundColor: "#1A1E1F",
+    }}
+  >
+    <Box
+      style={{
+        width: "686px",
+        height: "377px",
+        top: "176px",
+        left: "138px",
+        bottom: "471px",
+        right: "616px",
+        borderRadius: "40px",
+        backgroundColor: "#33373B5E",
+        position: "absolute",
+      }}
+    >
+      {/* Add content for the black rectangle if needed */}
+    </Box>
+    <Box
+      style={{
+        width: "417px",
+        height: "377px",
+        top: "176px",
+        left: "886px",
+        position: "absolute",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+      }}
+    >
+      <Heading
+        as="h2"
+        mb="4"
+        style={{
+          width: "123px",
+          height: "29px",
+          fontFamily: "Quicksand",
+          fontSize: "24px",
+          fontWeight: 700,
+          lineHeight: "29px",
+          letterSpacing: "0em",
+          textAlign: "left",
+          color: "#EFEEE0",
+        }}
+      >
+        Top Charts
+      </Heading>
+      {[1, 2, 3].map((index) => (
+        <Box
+          key={index}
+          style={{
+            width: "417px",
+            height: "96px",
+            borderRadius: "20px",
+            backgroundColor: "#1A1E1F",
+            marginBottom: "10px",
+          }}
+        >
+          {/* Add content for each of the three components as needed */}
+        </Box>
+      ))}
+    </Box>
+  </Flex>
+);
 
-      <Flex flexDirection="column" alignItems="down" p={5}>
-        <Text fontSize="4xl" fontWeight="bold">
-          Listen Anywhere
-        </Text>
-        <Text fontSize="xl" mt={2}>
-          Enjoy music on all your devices.
-        </Text>
-      </Flex>
-    </Container>
-  );
-};
+// Footer component
+const Footer = () => (
+  <Flex
+    as="footer"
+    align="center"
+    justify="center"
+    p="4"
+    bg="#1A1E1F"
+    color="white"
+    style={{
+      width: "1165px",
+      height: "236px",
+      top: "606px",
+      left: "138px",
+      padding: "0px 0px 2px 0px",
+      position: "absolute",
+    }}
+  >
+    <Text
+      style={{
+        width: "64px",
+        height: "29px",
 
-export default HomePage;
+        fontFamily: "Quicksand",
+        fontSize: "24px",
+        fontWeight: 700,
+        lineHeight: "29px",
+        letterSpacing: "0em",
+        textAlign: "left-bottom",
+        color: "#EFEEE0",
+      }}
+    >
+      Music
+    </Text>
+
+    <Flex
+      style={{
+        width: "100%",
+        justifyContent: "space-between",
+        gap: "5px",
+        position: "absolute",
+        top: "41px",
+      }}
+    >
+      {/* Replace this with your list of songs */}
+      {[1, 2, 3, 4].map((songIndex) => (
+        <Box
+          key={songIndex}
+          style={{
+            width: "153px",
+            height: "193px",
+            gap: "5px",
+          }}
+        >
+          {/* Add content for each song as needed */}
+        </Box>
+      ))}
+    </Flex>
+  </Flex>
+);
+
+// Homepage component
+const Homepage = () => (
+  <ChakraProvider theme={theme}>
+    <CSSReset />
+    <Box minH="100vh" bg="#1A1E1F">
+      {" "}
+      {/* Updated background color */}
+      <Header />
+      <MainSection />
+      <Footer />
+    </Box>
+  </ChakraProvider>
+);
+
+export default Homepage;
--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,51 +1,303 @@
+// Import necessary Chakra UI components
 import React from "react";
+import styled from "styled-components";
+
 import {
+  ChakraProvider,
   Box,
-  Container,
-  Avatar,
-  Text,
+  Flex,
   Heading,
-  Badge,
-  VStack,
-  HStack,
-  Button,
-  Stack,
+  Text,
+  extendTheme,
+  Link,
+  CSSReset,
+  Tooltip,
 } from "@chakra-ui/react";
-import useAuthStore from "../stores/authStore";
+// Import your image files
+import homeIcon from "./images/home.svg";
+import musicIcon from "./images/music.svg";
+import penIcon from "./images/pen.svg";
+import envelopeIcon from "./images/envelope.svg";
+import userIcon from "./images/user.svg";
 
-function ProfilePage() {
-  const { user, userID } = useAuthStore();
-  console.log(user);
-  const getUser = () => {
-    // send get request to get user info then proceed accordingly
-  };
-  return (
-    <Container maxW="xl" mt={16}>
-      <Box p={4} boxShadow="base" bg="white" borderRadius="md">
-        <Avatar size="xl" src="https://example.com/your-profile-image.jpg" />
-        <Heading as="h2" size="lg" mt={4}>
-          John Doe
-        </Heading>
+// Extend the theme if needed
+const theme = extendTheme(/* Your theme configurations here */);
+// Header component with custom images as icons
+const Header = () => (
+  <Flex
+    as="header"
+    align="center"
+    justify="space-between"
+    p="35"
+    bg="#1A1E1F"
+    color="white"
+  >
+    <Flex align="center">
+      {/* Home Link */}
+      <Link
+        mt="64px"
+        mr="12px"
+        ml="138px"
+        href="/"
+        borderRadius="15px"
+        width="64px"
+        height="64px"
+        bg="#33373B5E"
+        _hover={{ bg: "#FFFFFF" }}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Tooltip label="Home" placement="bottom">
+          <img src={homeIcon} alt="Home" style={{ width: "100%" }} />
+        </Tooltip>
+      </Link>
 
-        <VStack spacing={4} mt={4}>
-          <Text fontSize="md">
-            Hi, I'm John Doe, a passionate front-end developer with a strong
-            focus on web technologies.
-          </Text>
-          <Text fontSize="md">
-            Connect with me on social media to stay updated with my latest
-            projects and thoughts on web development.
-          </Text>
-        </VStack>
-        <HStack spacing={4} mt={4}>
-          <Button colorScheme="teal">Add Friend</Button>
-          <Button variant="outline" colorScheme="teal">
-            Message
-          </Button>
-        </HStack>
+      {/* Recommend a Song Link */}
+      <Link
+        mt="64px"
+        href="/recommend"
+        borderRadius="15px"
+        width="64px"
+        height="64px"
+        bg="#33373B5E"
+        _hover={{ bg: "#FFFFFF" }}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Tooltip label="Recommend a Song" placement="bottom">
+          <img src={musicIcon} alt="Music" style={{ width: "100%" }} />
+        </Tooltip>
+      </Link>
+
+      {/* Search Bar */}
+      <Box flex="1" display="flex" alignItems="center" justifyContent="center">
+        <Link
+          mt="64px"
+          ml="12px"
+          mr="12px"
+          href="/search"
+          borderRadius="15px"
+          width="785px"
+          height="64px"
+          bg="#33373B5E"
+          _hover={{ bg: "#FFFFFF" }}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Tooltip label="Search" placement="bottom">
+            <div
+              style={{
+                width: "0px",
+                height: "40px",
+                backgroundColor: "#33373B5E",
+              }}
+            />
+          </Tooltip>
+        </Link>
       </Box>
-    </Container>
-  );
-}
 
-export default ProfilePage;
+      {/* Create Song Link */}
+      <Link
+        mr="12px"
+        mt="64px"
+        href="/create"
+        borderRadius="15px"
+        width="64px"
+        height="64px"
+        bg="#33373B5E"
+        _hover={{ bg: "#FFFFFF" }}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Tooltip label="Create Song" placement="bottom">
+          <img src={penIcon} alt="Pen" style={{ width: "100%" }} />
+        </Tooltip>
+      </Link>
+
+      {/* Direct Message Link */}
+      <Link
+        mt="64px"
+        mr="12px"
+        href="/messages"
+        borderRadius="15px"
+        width="64px"
+        height="64px"
+        bg="#33373B5E"
+        _hover={{ bg: "#FFFFFF" }}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Tooltip label="Direct Message" placement="bottom">
+          <img src={envelopeIcon} alt="Envelope" style={{ width: "100%" }} />
+        </Tooltip>
+      </Link>
+
+      {/* My Profile Link */}
+      <Link
+        mt="64px"
+        mr="137px"
+        href="/profile"
+        borderRadius="15px"
+        width="64px"
+        height="64px"
+        bg="#33373B5E"
+        _hover={{ bg: "#FFFFFF" }}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Tooltip label="My Profile" placement="bottom">
+          <img src={userIcon} alt="User" style={{ width: "100%" }} />
+        </Tooltip>
+      </Link>
+    </Flex>
+  </Flex>
+);
+const MainSection = () => (
+  <Flex
+    as="main"
+    align="center"
+    justify="center"
+    p="4"
+    style={{
+      backgroundColor: "#1A1E1F",
+    }}
+  >
+    {/* Name-Surname */}
+    <Box
+      style={{
+        width: "349px",
+        height: "58px",
+        fontFamily: "Quicksand",
+        fontSize: "48px",
+        fontWeight: 400,
+        lineHeight: "58px",
+        letterSpacing: "0em",
+        textAlign: "left",
+        color: "#FFFFFF",
+        position: "absolute",
+        left: "419px",
+        top: "192px",
+      }}
+    >
+      Name-Surname
+    </Box>
+
+    {/* Username */}
+    <Box
+      style={{
+        width: "135px",
+        height: "29px",
+        opacity: 0.5,
+        fontFamily: "Quicksand",
+        fontSize: "24px",
+        fontWeight: 400,
+        lineHeight: "29px",
+        letterSpacing: "0em",
+        textAlign: "left",
+        color: "#FFFFFF",
+        position: "absolute",
+        left: "418px",
+        top: "250px",
+      }}
+    >
+      username
+    </Box>
+
+    {/* Profile Photo */}
+    <Box
+      style={{
+        width: "256px",
+        height: "256px",
+        top: "192px",
+        left: "138px",
+        borderRadius: "8.53px",
+        position: "absolute",
+      }}
+    >
+      {/* Add content for the profile photo if needed */}
+    </Box>
+  </Flex>
+);
+
+// Footer component
+const Footer = () => (
+  <Flex
+    as="footer"
+    align="center"
+    justify="center"
+    p="4"
+    bg="#1A1E1F"
+    color="white"
+    style={{
+      width: "1165px",
+      height: "236px",
+      top: "606px",
+      left: "138px",
+      padding: "0px 0px 2px 0px",
+      position: "absolute",
+    }}
+  >
+    <Text
+      style={{
+        width: "64px",
+        height: "29px",
+
+        fontFamily: "Quicksand",
+        fontSize: "24px",
+        fontWeight: 700,
+        lineHeight: "29px",
+        letterSpacing: "0em",
+        textAlign: "left-bottom",
+        color: "#EFEEE0",
+      }}
+    >
+      Music
+    </Text>
+
+    <Flex
+      style={{
+        width: "100%",
+        justifyContent: "space-between",
+        gap: "5px",
+        position: "absolute",
+        top: "41px",
+      }}
+    >
+      {/* Replace this with your list of songs */}
+      {[1, 2, 3, 4].map((songIndex) => (
+        <Box
+          key={songIndex}
+          style={{
+            width: "153px",
+            height: "193px",
+            gap: "5px",
+          }}
+        >
+          {/* Add content for each song as needed */}
+        </Box>
+      ))}
+    </Flex>
+  </Flex>
+);
+
+// Homepage component
+const Homepage = () => (
+  <ChakraProvider theme={theme}>
+    <CSSReset />
+    <Box minH="100vh" bg="#1A1E1F">
+      {" "}
+      {/* Updated background color */}
+      <Header />
+      <MainSection />
+      <Footer />
+    </Box>
+  </ChakraProvider>
+);
+
+export default Homepage;
