b947e76db2a09b163423a7efddc81285c9dba66f
Ayça Elif Aktaş <aycaaktas@sabanciuniv.edu>
2023-11-29 00:51:00 +0300

    music-alchemy/.vscode/launch.json

--- my-express-app/create-song.js
+++ my-express-app/create-song.js
@@ -12,44 +12,67 @@ admin.initializeApp({
     //databaseURL: 'https://your-database-url.firebaseio.com' // Replace with your Firebase DB URL
   });
 
-  const db = admin.firestore(); 
+const db = admin.firestore(); 
 //set up express app
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-async function getAccessToken(code) {
-  const clientId = '046cbc3fe1ca4bef9a2578dbd5d219ec';
-  const clientSecret = '8d90011f8474f0f933748c9b7d43b54';
-  const redirectUri = 'http://localhost/';
+
+
+const querystring = require('querystring');
+
+const clientId = '57e35b7900e2473eb2c8a3dc6b3e68ce';
+const clientSecret = 'ee8e317543da4aa59bb351217a476e14';
+const redirectUri = 'http://localhost:3000/callback';
+
+app.get('/login', (req, res) => {
+  const scopes = 'user-read-private user-read-email'; // Add necessary scopes
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: clientId,
+      scope: scopes,
+      redirect_uri: redirectUri,
+    }));
+});
+
+
+let accessToken;
+
+app.get('/callback', async (req, res) => {
+  const code = req.query.code || null;
 
   try {
-    const response = await axios.post('https://accounts.spotify.com/api/token', null, {
-      params: {
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: redirectUri,
-      },
+    const response = await axios.post('https://accounts.spotify.com/api/token', querystring.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: redirectUri,
+    }), {
       headers: {
-        'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
         'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
       },
     });
 
-    return response.data.access_token;
-  } catch (error) {
-    console.error('Error getting access token:', error);
-    throw error;
-  }
-}
+    const accessToken = response.data.access_token;
 
-// endpoint song request
+    // Now you have the accessToken, you can use it in subsequent routes or functions
+  
+
+  } catch (error) {
+    console.error('Error exchanging code for token:', error.message);
+    res.status(500).send('Error exchanging code for token');
+  }
+});
+
+
+
 app.get('/autocomplete', async (req, res) => {
   
   const query = req.query.q;
   
   try {
-    const accessToken = await getAccessToken();
     const apiUrl = 'https://api.spotify.com/v1/search';
     const response = await axios.get(apiUrl, {
       headers: {
@@ -69,13 +92,14 @@ app.get('/autocomplete', async (req, res) => {
   }
 });
 
-app.get('/search-track', async (req, res) => {
+
+
+app.post('/create-track', async (req, res) => {
   
   const trackName = req.query.trackName;
 
   try {
-    // Get access token from Spotify (you may need to implement this function)
-    const accessToken = await getAccessToken();
+    // Get access token from Spotify (you may need to implement this function
     const apiUrl = 'https://api.spotify.com/v1/search';
     // Make a request to Spotify API to search for the track
     const response = await axios.get(apiUrl, {
@@ -90,19 +114,77 @@ app.get('/search-track', async (req, res) => {
 
     const track = response.data.tracks.items[0]; // Assuming you want only the first track
 
-    // Add the returned track to your Firebase database
-    const db = admin.firestore();
-    const tracksCollection = db.collection('added_tracks');
+    if (track) {
+      // Check if the track already exists in the 'Tracks' collection
+      const trackId = track.id;
+      const query = db.collection('Tracks').where('spotify_track_id', '==', trackId).limit(1);
+      const existingTrackSnapshot = await query.get();
+      const trackExists = !existingTrackSnapshot.empty;
 
-    await tracksCollection.add(track);
+      if (!trackExists) {
+        // Process track data and add it to the 'Tracks' collection
+        // ... (your existing track data processing code)
+        const albumId = track.album.id;
 
-    res.json(track); // Adjust the response format as needed
+        // Fetch audio features for the track
+        const audioFeatures = await getAccessToken.getAudioFeaturesForTrack(trackId);
+
+        const artistNames = [];
+        const artistUrls = [];
+        const artistIds = [];
+        const artistImages = [];
+
+        // Process each artist in the track
+        for (const artist of track.artists) {
+          artistNames.push(artist.name);
+          artistUrls.push(artist.external_urls.spotify);
+          artistIds.push(artist.id);
+
+          // Fetch artist information to get images
+          const artistInfo = await getAccessToken.getArtist(artist.id);
+          artistImages.push(...artistInfo.body.images);
+        }
+
+        // Create the track data object
+        const trackData = {
+          album_type: track.album.album_type,
+          album_URL: track.album.external_urls.spotify,
+          album_name: track.album.name,
+          album_release_date: track.album.release_date,
+          album_images: track.album.images, // array
+          spotify_album_id: albumId,
+          artists: artistNames, // array
+          artist_urls: artistUrls, // array
+          artist_images: artistImages, // array
+          spotify_artist_ids: artistIds, // array
+          length_in_seconds: track.duration_ms / 1000,
+          spotify_track_id: trackId,
+          track_url: track.external_urls.spotify,
+          track_name: track.name,
+          rating: 0.0,
+          like_count: 0,
+          rating_count: 0,
+          added_at: admin.firestore.FieldValue.serverTimestamp(),
+          acousticness: audioFeatures.body.acousticness,
+          danceability: audioFeatures.body.danceability,
+          energy: audioFeatures.body.energy,
+          instrumentalness: audioFeatures.body.instrumentalness,
+          liveness: audioFeatures.body.liveness,
+          loudness: audioFeatures.body.loudness,
+          mode: audioFeatures.body.mode,
+          tempo: audioFeatures.body.tempo,
+          key: audioFeatures.body.key,
+          valence: audioFeatures.body.valence,
+        };
+
+        // Add the track data to the 'Tracks' collection
+        await db.collection('Tracks').add(trackData);
+      }
+    }
+
+    res.json(trackData); // Adjust the response format as needed
   } catch (error) {
     console.error('Error searching for track:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
-});
-const PORT = process.env.PORT || 3010;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
