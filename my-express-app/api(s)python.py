1e7307e882f11320e07493de18744db6e5b365e4
Ayça Elif Aktaş <aycaaktas@sabanciuniv.edu>
2023-12-08 13:37:14 +0300

    creat api change

--- my-express-app/api(s)python.py
+++ my-express-app/api(s)python.py
@@ -101,6 +101,7 @@ def autocomplete():
             'track_name': track['name'],
             'artist(s)': artist_names,
             'album_name': track['album']['name'],
+            'spotify_track_id':track['id']
         }
         suggestions.append(track_info)
 
@@ -114,83 +115,88 @@ def autocomplete():
 def create_song():
     # Get the track name from the request body
     data = request.get_json()
-    track_name = data.get('track_name')
+    track_spotify_id = data.get('track_spotify_id')
 
     # Search for the track
 
-    track_results = sp.search(q=track_name, type='track', limit=1)
-    tracks = track_results['tracks']['items']
+    track = sp.track(track_spotify_id)
+    
 
  
-    for track in tracks:
-        # Extract relevant information about the track
-        track_id=track['id']
-        field = 'spotify_track_id'
-        op = '=='
-        value = track_id
-        query = db.collection('Tracks')
-        query = query.where(filter=FieldFilter(field, op, value)).limit(1)
-        existing_track = query.stream()
-        track_exists = len(list(existing_track)) > 0
+    track_id=track['id']
+    field = 'spotify_track_id'
+    op = '=='
+    value = track_id
+    query = db.collection('Tracks')
+    query = query.where(filter=FieldFilter(field, op, value)).limit(1)
+    existing_track = query.stream()
+    track_exists = len(list(existing_track)) > 0
 
-        if not track_exists:
+    if not track_exists:
 
-            album_id=track['album']['id']
-            audio_features = sp.audio_features(track_id)
+        album_id=track['album']['id']
+        audio_features = sp.audio_features(track_id)
                 
-            artist_names = []
-            artist_urls = []
-            artist_ids =[]
-            artist_images= []
+        artist_names = []
+        artist_urls = []
+        artist_ids =[]
+        artist_images= []
                 
                 
-            for artist in track['artists']:
-                artist_names.append(artist['name'])
-                artist_urls.append(artist['external_urls']['spotify'])
-                artist_ids.append(artist['id'])
+        for artist in track['artists']:
+            artist_names.append(artist['name'])
+            artist_urls.append(artist['external_urls']['spotify'])
+            artist_ids.append(artist['id'])
                 
 
-            for id in artist_ids:
-                artist_info=sp.artist(id)
-                for image_info in artist_info['images']:
-                    artist_images.append(image_info)
+        for id in artist_ids:
+            artist_info=sp.artist(id)
+            for image_info in artist_info['images']:
+                artist_images.append(image_info)
                     
-                
-            track_data = {
-                'album_type': track['album']['album_type'],
-                'album_URL': track['album']['external_urls']['spotify'],
-                'album_name': track['album']['name'],
-                'album_release_date': track['album']['release_date'],
-                'album_images':track['album']['images'],#array
-                'spotify_album_id':album_id,
-                'artists':artist_names, #array
-                'artist_urls':artist_urls, #array
-                'artist_images':artist_images,#array
-                'spotify_artist_id(s)':artist_ids,#array
-                'length_in_seconds':track['duration_ms']/1000,
-                'spotify_track_id':track_id,
-                'track_url':track['external_urls']['spotify'],
-                'track_name':track['name'],
-                'rating':0.0,
-                'like_count':0,
-                'rating_count':0,
-                'added_at': firestore.SERVER_TIMESTAMP, 
-                'acousticness':audio_features[0]['acousticness'],
-                'danceability':audio_features[0]['danceability'],
-                'energy':audio_features[0]['energy'], # Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity
-                'instrumentalness':audio_features[0]['instrumentalness'],
-                'liveness':audio_features[0]['liveness'],  #is the track live
-                'loudness':audio_features[0]['loudness'],
-                'mode':audio_features[0]['mode'], # Major is represented by 1 and minor is 0
-                'tempo':audio_features[0]['tempo'],# The overall estimated tempo of a track in beats per minute (BPM)
-                'key':audio_features[0]['key'], # The key the track is in
-                'valence':audio_features[0]['valence'] # Tracks with high valence sound more positive , while tracks with low valence sound more negative
+        lowercased_track_name = track['name'].lower()   
+        lower_artists=[]
+        for artist in artist_names:
+                    artist_lower = artist.lower() 
+                    lower_artists.append(artist_lower) 
+
+        track_data = {
+            'album_type': track['album']['album_type'],
+            'album_URL': track['album']['external_urls']['spotify'],
+            'album_name': track['album']['name'],
+            'album_release_date': track['album']['release_date'],
+            'album_images':track['album']['images'],#array
+            'spotify_album_id':album_id,
+            'artists':artist_names, #array
+            'artist_urls':artist_urls, #array
+            'artist_images':artist_images,#array
+            'spotify_artist_id(s)':artist_ids,#array
+            'length_in_seconds':track['duration_ms']/1000,
+            'spotify_track_id':track_id,
+            'track_url':track['external_urls']['spotify'],
+            'track_name':track['name'],
+            'lowercased_artists': lower_artists,
+            'lowercased_track_name': lowercased_track_name,
+            'rating':0.0,
+            'like_count':0,
+            'rating_count':0,
+            'added_at': firestore.SERVER_TIMESTAMP, 
+            'acousticness':audio_features[0]['acousticness'],
+            'danceability':audio_features[0]['danceability'],
+            'energy':audio_features[0]['energy'], # Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity
+            'instrumentalness':audio_features[0]['instrumentalness'],
+            'liveness':audio_features[0]['liveness'],  #is the track live
+            'loudness':audio_features[0]['loudness'],
+            'mode':audio_features[0]['mode'], # Major is represented by 1 and minor is 0
+            'tempo':audio_features[0]['tempo'],# The overall estimated tempo of a track in beats per minute (BPM)
+            'key':audio_features[0]['key'], # The key the track is in
+            'valence':audio_features[0]['valence'] # Tracks with high valence sound more positive , while tracks with low valence sound more negative
             }
-            db.collection('Tracks').add(track_data)
-            return jsonify({'success': True, 'message': f'Song "{track_name}" saved to Firestore'})
+        db.collection('Tracks').add(track_data)
+        return jsonify({'success': True, 'message': f'Song "{track['name']}" saved to Firestore'})
             
-        else:
-            return jsonify({'success': False, 'message': 'Track already exist in database'})
+    else:
+        return jsonify({'success': False, 'message': 'Track already exist in database'})
 
 
 
