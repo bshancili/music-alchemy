02c7f6ca6fca03a1519b975d74ba292e708c858e
mvsbuse <mavisbalci@sabanciuniv.edu>
2024-01-21 20:20:30 +0300

    recent_liked_songs endpoint last version

--- my-express-app/test/recent_liked_songs.test.js
+++ my-express-app/test/recent_liked_songs.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 const app = require('/Users/mvsbuse/music-alchemy-updated/music-alchemy/my-express-app/server.js'); 
 
-describe('/retrieve_user_tracks endpoint', () => {
+describe('/recent_liked_songs endpoint', () => {
   // Test for valid UID
-  it('should return user tracks for a valid UID', async () => {
-    const validUid = 'm2y0Gj0PwxegLb8DaSDE5sQ75oC3'; // a valid UID 
+  it('should return recent liked songs for a valid UID', async () => {
+    const validUid = 'm2y0Gj0PwxegLb8DaSDE5sQ75oC3'; 
     const response = await request(app)
-      .post('/retrieve_user_tracks')
+      .post('/recent_liked_songs')
       .send({ uid: validUid });
 
     expect(response.statusCode).toBe(200);
@@ -15,9 +15,9 @@ describe('/retrieve_user_tracks endpoint', () => {
 
   // Test for non-existent UID
   it('should return a 404 error for non-existent UID', async () => {
-    const nonExistentUid = '45678ioedfgh';
+    const nonExistentUid = 'nonExistentUid';
     const response = await request(app)
-      .post('/retrieve_user_tracks')
+      .post('/recent_liked_songs')
       .send({ uid: nonExistentUid });
 
     expect(response.statusCode).toBe(404);
@@ -25,12 +25,12 @@ describe('/retrieve_user_tracks endpoint', () => {
   });
 
   // Test for missing UID
-  it('should return a 404 error when no UID is provided', async () => {
+  it('should return a 400 error when no UID is provided', async () => {
     const response = await request(app)
-      .post('/retrieve_user_tracks')
+      .post('/recent_liked_songs')
       .send({});
 
-    expect(response.statusCode).toBe(404);
+    expect(response.statusCode).toBe(400);
     expect(response.body).toEqual({ error: "UID is required" });
   });
 });
